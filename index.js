399a6b0c58b72df35da71f0d4e060b23095640ca
Alena <alyonatihomirova9@gmail.com>
2023-12-08 00:47:32 +0300

    update

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ let orderArray = []
 
 document.addEventListener("click", function(e) {
     if (e.target.dataset.add) {
-        console.log(e.target.dataset.add)
         handleAddButtonClick(e.target.dataset.add)
+    } else if (e.target.dataset.remove) {
+        handleRemoveButtonClick(e.target.dataset.remove)
+    } else if (e.target.id === "purchase-button") {
+        handlePurchaseButtonClick()
+    } else if (e.target.id === "pay-button") {
+        handlePayButtonClick()
     }
 })
 
@@ -16,7 +21,72 @@ function handleAddButtonClick(itemId) {
     })[0]
 
     orderArray.push(targetItemObj)
-    console.log(orderArray)
+
+    document.getElementById("checkout-section").style.display = "flex"
+
+    renderOrder()
+
+}
+
+function handleRemoveButtonClick(itemId) {
+    console.log("clicked" + itemId)
+
+    const targetItemObj = menuArray.filter( function(menuItem) {
+        return menuItem.id === itemId
+    })[0]
+
+    orderArray.pop(targetItemObj)
+
+    if (orderArray.length > 0) {
+        renderOrder()
+    } else {
+        document.getElementById("checkout-section").style.display = "none"
+    }
+
+}
+
+function renderOrder() {
+    
+    let orderHtml = ""
+
+    let totalPrice = []
+
+    orderArray.forEach( function(orderItem) {
+
+        orderHtml +=`
+        <div class="order-item">
+            <h3>${orderItem.name}</h3>
+            <button class="remove-button" data-remove="${orderItem.id}">remove</button>
+            <div class="order-item-price">
+                <h4>$${orderItem.price}</h4>
+            </div>
+        </div>`
+
+        totalPrice.push(orderItem.price)
+    })
+
+    document.getElementById("order-items").innerHTML = orderHtml
+
+    const sumToPay = totalPrice.reduce( function(total, currentPrice) {
+        return total + currentPrice
+    })
+
+    document.getElementById("order-sum").innerHTML = `<h4>$${sumToPay}</h4>`
+}
+
+function handlePurchaseButtonClick() {
+    document.getElementById("payment-modal").style.display = "flex"
+}
+
+function handlePayButtonClick() {
+    document.getElementById("payment-modal").style.display = "none"
+
+    document.getElementById("checkout-section").innerHTML = `
+        <div class="order-complete-message-section"
+            id="order-complete-message-section">
+            <h8>Thanks, James! Your order is on its way!</h8>
+        </div>`
+
 }
 
 function getMenuHtml() {
