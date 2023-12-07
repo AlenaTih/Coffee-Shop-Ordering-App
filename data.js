463d27cccbb06a1880057e330988b9363c087bb7
Alena <alyonatihomirova9@gmail.com>
2023-12-07 12:53:09 +0300

    update

--- data.js
+++ data.js
@@ -4,21 +4,21 @@ const menuArray = [
         ingredients: ["pepperoni", "mushrooms", "mozarella"],
         price: 14,
         emoji: "🍕",
-        id: 0
+        id: "0"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
         emoji: "🍔",
-        id: 1
+        id: "1"
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: "2"
     }
 ]
 
--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 import menuArray from '/data.js'
 
+let orderArray = []
+
+document.addEventListener("click", function(e) {
+    if (e.target.dataset.add) {
+        console.log(e.target.dataset.add)
+        handleAddButtonClick(e.target.dataset.add)
+    }
+})
+
+function handleAddButtonClick(itemId) {
+
+    const targetItemObj = menuArray.filter( function(menuItem) {
+        return menuItem.id === itemId
+    })[0]
+
+    orderArray.push(targetItemObj)
+    console.log(orderArray)
+}
+
 function getMenuHtml() {
      
     let menuHtml = ""
@@ -7,17 +26,17 @@ function getMenuHtml() {
     menuArray.forEach( function(item) {
 
         menuHtml +=`
-        <div class="menu-item" id="${item.id}">
+        <div class="menu-item"">
             <div class="item-image">
                 <h2>${item.emoji}</h2>
             </div>
             <div>
                 <h3>${item.name}</h3>
-                <p>${item.ingredients}</p>
+                <p>${item.ingredients.join(", ")}</p>
                 <h4>$${item.price}</h4>
             </div>
             <div class="add-btn-container">
-                <button class="add-button">
+                <button class="add-button" data-add="${item.id}">
                     <i class="fa-light fa-plus" data-add="${item.id}"></i>
                 </button>
             </div>
