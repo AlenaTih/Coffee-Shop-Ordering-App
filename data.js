3e11e9140ea6a79f8dd02ea547016f3c5a802159
Alena <alyonatihomirova9@gmail.com>
2023-12-12 21:56:50 +0300

    update

--- data.js
+++ data.js
@@ -3,42 +3,42 @@ const menuArray = [
         name: "Cappuccino",
         ingredients: ["A classic Italian favourite made with rich espresso, steamed milk, and a layer of foamy froth"],
         price: 14,
-        image: "coffee.png",
+        image: "cappuccino.png",
         id: "0"
     },
     {
         name: "Latte",
         ingredients: ["A velvety-smooth blend of espresso and steamed milk, topped with a generous swirl of froth"],
         price: 15,
-        image: "coffee.png",
+        image: "latte.png",
         id: "1"
     },
         {
         name: "Americano",
         ingredients: ["A milder take on the traditional espresso, made by adding hot water to a shot of espresso, creating a smooth and balanced cup that’s perfect for any time of day"],
         price: 12,
-        image: "coffee.png",
+        image: "americano.png",
         id: "2"
     },
     {
         name: "Espresso",
         ingredients: ["For those who like their coffee strong and intense, this drink is a concentrated shot of pure flavor, served straight up"],
         price: 9,
-        image: "coffee.png",
+        image: "espresso.png",
         id: "3"
     }
     // {
     //     name: "Hamburger",
     //     ingredients: ["beef", "cheese", "lettuce"],
     //     price: 12,
-    //     image: "coffee.png",
+    //     image: "cappuccino.png",
     //     id: "1"
     // },
     //     {
     //     name: "Beer",
     //     ingredients: ["grain, hops, yeast, water"],
     //     price: 12,
-    //     image: "coffee.png",
+    //     image: "cappuccino.png",
     //     id: "2"
     // }
 ]
