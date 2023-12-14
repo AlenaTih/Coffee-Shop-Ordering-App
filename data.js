fae5d7c5480aa5bc040eb5ae5555839e1c167226
Alena <alyonatihomirova9@gmail.com>
2023-12-14 18:19:09 +0300

    update

--- data.js
+++ data.js
@@ -1,42 +1,9 @@
-// const menuArray = [
-//     {
-//         name: "Cappuccino",
-//         ingredients: ["A classic Italian favourite made with rich espresso, steamed milk, and a layer of foamy froth"],
-//         price: 14,
-//         image: "cappuccino.png",
-//         id: "0"
-//     },
-//     {
-//         name: "Latte",
-//         ingredients: ["A velvety-smooth blend of espresso and steamed milk, topped with a generous swirl of froth"],
-//         price: 15,
-//         image: "latte.png",
-//         id: "1"
-//     },
-//         {
-//         name: "Americano",
-//         ingredients: ["A milder take on the traditional espresso, made by adding hot water to a shot of espresso, creating a smooth and balanced cup that’s perfect for any time of day"],
-//         price: 12,
-//         image: "americano.png",
-//         id: "2"
-//     },
-//     {
-//         name: "Espresso",
-//         ingredients: ["For those who like their coffee strong and intense, this drink is a concentrated shot of pure flavor, served straight up"],
-//         price: 9,
-//         image: "espresso.png",
-//         id: "3"
-//     }
-// ]
-
-// export default menuArray
-
 const menuArray = [
     {
       name: "Cappuccino",
       ingredients: ["A classic Italian favourite made with rich espresso, steamed milk, and a layer of foamy froth"],
       sizes: ["Small", "Medium", "Big"],
-      prices: [14, 16, 18],
+      prices: [3, 4, 5],
       image: "cappuccino.png",
       id: "0"
     },
@@ -44,7 +11,7 @@ const menuArray = [
       name: "Latte",
       ingredients: ["A velvety-smooth blend of espresso and steamed milk, topped with a generous swirl of froth"],
       sizes: ["Small", "Medium", "Big"],
-      prices: [15, 17, 19],
+      prices: [4, 5, 6],
       image: "latte.png",
       id: "1"
     },
@@ -52,7 +19,7 @@ const menuArray = [
       name: "Americano",
       ingredients: ["A milder take on the traditional espresso, made by adding hot water to a shot of espresso, creating a smooth and balanced cup that’s perfect for any time of day"],
       sizes: ["Small", "Medium", "Big"],
-      prices: [12, 14, 16],
+      prices: [2, 3, 3.50],
       image: "americano.png",
       id: "2"
     },
@@ -60,7 +27,7 @@ const menuArray = [
       name: "Espresso",
       ingredients: ["For those who like their coffee strong and intense, this drink is a concentrated shot of pure flavor, served straight up"],
       sizes: ["Small", "Medium", "Big"],
-      prices: [9, 11, 13],
+      prices: [2, 2.50, 3],
       image: "espresso.png",
       id: "3"
     }
